8c26553cbc8df1b81a612f2cbec80d88e1e2e0f2
g-greene <118185951+g-greene@users.noreply.github.com>
2024-02-18 15:03:59 -0500

    More updates to work with Netlify

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,10 @@
-/**
- * @type {import('@remix-run/dev/config').AppConfig}
- */
-module.exports = {
-  appDirectory: "app",
-  tailwind: true,
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
-  devServerPort: 8002,
-  ignoredRouteFiles: [".*"],
-} 
+import { config } from "@netlify/remix-adapter";
+
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ...(process.env.NODE_ENV === "production" ? config : undefined),
+  // This works out of the box with the Netlify adapter, but you can
+  // add your own custom config here if you want to.
+  //
+  // See https://remix.run/file-conventions/remix-config
+};
