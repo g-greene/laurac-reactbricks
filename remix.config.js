829cd82513d3147425243582e176655c634abd32
g-greene <118185951+g-greene@users.noreply.github.com>
2024-02-18 16:11:32 -0500

    Added react-bricks to remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,7 @@ import { config } from "@netlify/remix-adapter";
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ...(process.env.NODE_ENV === "production" ? config : undefined),
-  serverDependenciesToBundle: [/^pigeon-maps/]
+  serverDependenciesToBundle: [/^pigeon-maps/,/^react-bricks/]
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
   //
