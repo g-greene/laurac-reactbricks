87e554c6cc019114dedec928ea0fc2f2a69ca446
g-greene <118185951+g-greene@users.noreply.github.com>
2024-02-18 17:00:16 -0500

    Added tailwind and postcss to remix.config.js; added other files

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,9 @@ import { config } from "@netlify/remix-adapter";
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ...(process.env.NODE_ENV === "production" ? config : undefined),
-  serverDependenciesToBundle: [/^pigeon-maps/,/^react-bricks/,/^react-lazy-load-image-component/,/^react-dropzone/,/^prismjs/]
+  serverDependenciesToBundle: [/^pigeon-maps/,/^react-bricks/,/^react-lazy-load-image-component/,/^react-dropzone/,/^prismjs/,/^remix-run/],
+  tailwind: true,
+  postcss: true
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
   //
